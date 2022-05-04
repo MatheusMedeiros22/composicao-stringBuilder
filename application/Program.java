11268dbaabee8124d579c1ae38810e00ff04f99b
MatheusMedeiros22 <mathmed22@gmail.com>
2022-05-04 19:17:05 -0300

    Aprendendo a utilizar o String Builder

--- application/Program.java
+++ application/Program.java
@@ -1,10 +1,41 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+
+import entities.Comment;
+import entities.Post;
+
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		// TODO Auto-generated method stub
-
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		Scanner sc = new Scanner(System.in);
+		
+		Comment c1 = new Comment("Aprender a programar em java é muito bom");
+		Comment c2 = new Comment("Procuro pela minha primeira oportunidade de emprego na área de TI");
+		
+		
+		
+		Post p1 = new Post(sdf.parse("04/05/2022 19:00:00"), 
+				"Aprendendo a programar", 
+				"Estou focado no aprendizado em java a 3 semanas", 
+				22);
+		
+		p1.addComment(c1);
+		p1.addComment(c2);
+		
+		System.out.println(p1);
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
 	}
 
 }
--- entities/Post.java
+++ entities/Post.java
@@ -1,16 +1,20 @@
 package entities;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 public class Post {
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	
 	private Date moment;
 	private String title;
 	private String content;
 	private Integer likes;
 	
-	List<Comment> comments = new ArrayList<>();
+	private List<Comment> comments = new ArrayList<>();
 	
 	public Post () {
 		
@@ -55,7 +59,29 @@ public class Post {
 		this.likes = likes;
 	}
 	
+	public void addComment(Comment comment) {
+		comments.add(comment);
+	}
 	
+	public void removeComment(Comment comment) {
+		comments.remove(comment);
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(title + "\n");
+		sb.append(likes);
+		sb.append(" Likes - ");
+		sb.append(sdf.format(moment) + "\n");
+		sb.append(content + "\n");
+		sb.append("Comments:\n");
+		for(Comment comment : comments) {
+			sb.append(comment.getText() + "\n");
+		}
+		return sb.toString();
+				
+	}
 	
 	
 }
